2ac511872ca6db67b9e35188fcc4ee3cf6e21cb7
CheetoBuri <khoiphamleanh67@gmail.com>
2025-04-16 23:06:47 -0500

    Updated Program.cs

--- SchoolAppCore/Program.cs
+++ SchoolAppCore/Program.cs
@@ -7,15 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SchoolContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
 app.UseStaticFiles();
 app.UseRouting();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
